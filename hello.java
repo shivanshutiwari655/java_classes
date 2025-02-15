4d1c3d344001194443e9038a8c056b997fdebaaf
Shivanshu Tiwari <shuvanshutiwarishivanshu@gmail.com>
2025-02-15 11:56:36 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+
+    public static void main(String[] args) {
+        String name="shivanshu tiwari";
+        System.out.println("Hello world wellcome mr. "+ name);
+        System.out.println(name+6);
+        System.out.println(2+4);
+    }
+}
